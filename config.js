c2821189e414a84d541d6e733cb0a84b0aec88a1
Avaer Kazmer <a@modules.io>
2020-06-10 16:47:56 -0400

    Bugfix port conflict in config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ const config =
 	{
 		listenIp   : '0.0.0.0',
 		// NOTE: Don't change listenPort (client app assumes 4443).
-		listenPort : process.env.ADMIN_LISTEN_PORT || 7000
+		listenPort : process.env.ADMIN_LISTEN_PORT || 4444
 	},
 	// mediasoup settings.
 	mediasoup :
