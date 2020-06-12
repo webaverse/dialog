173d03d997fc9ff964c0978bfd9992716e72c205
Avaer Kazmer <a@modules.io>
2020-06-12 15:55:25 -0400

    File moving

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ pushd mediasoup-client
 parcel build --target=browser lib/index.js
 popd
 
-pushd protoo-client
+pushd client/protoo-client
 NODE_ENV=development parcel build --target=browser lib/index.js # --no-minify
 cat ../prefix.js dist/index.js ../suffix.js >build.js
 js-beautify -r build.js
 popd
 
-pushd mediasoup-client
+pushd client/mediasoup-client
 NODE_ENV=development parcel build --target=browser lib/index.js # --no-minify
 cat ../prefix.js dist/index.js ../suffix.js >build.js
 js-beautify -r build.js
