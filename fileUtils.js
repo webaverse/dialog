96a7332281a375d6651e2966c3c902278ecca810
Chris <chris.eddy.dev@gmail.com>
2020-08-24 22:12:28 -0700

    remove old lock code and function

--- fileUtils.js
+++ fileUtils.js
@@ -1,16 +1,13 @@
 const fs = require('fs');
-const lockMap = new Map();
 
 const writeFile = (path, src) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (lockMap.get(key) && !lockMap.get(key).locked) {
-                const fileHandle = await fs.promises.open(`${process.cwd()}/chunkSrc/${path}`, 'w+');
-                if (fileHandle) {
-                    await fs.promises.writeFile(fileHandle, src);
-                    await fileHandle.close();
-                    resolve();
-                }
+            const fileHandle = await fs.promises.open(`${process.cwd()}/chunkSrc/${path}`, 'w+');
+            if (fileHandle) {
+                await fs.promises.writeFile(fileHandle, src);
+                await fileHandle.close();
+                resolve();
             } else {
                 console.error('failed to write to file')
                 reject()
@@ -30,16 +27,14 @@ const getAllKeys = async () => {
 const getFile = (key) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (lockMap.get(key) && !lockMap.get(key).locked) {
-                const fileHandle = await fs.promises.open(`${process.cwd()}/chunkSrc/${key}`, 'r');
-                if (fileHandle) {
-                    const buffer = await fs.promises.readFile(fileHandle);
-                    if (buffer) {
-                        resolve(buffer);
-                    }
-                    resolve('');
-                    await fileHandle.close();
+            const fileHandle = await fs.promises.open(`${process.cwd()}/chunkSrc/${key}`, 'r');
+            if (fileHandle) {
+                const buffer = await fs.promises.readFile(fileHandle);
+                if (buffer) {
+                    resolve(buffer);
                 }
+                resolve('');
+                await fileHandle.close();
             } else {
                 console.error('cant get file')
                 reject()
@@ -51,21 +46,8 @@ const getFile = (key) => {
     })
 }
 
-const evalSrc = (src) => {
-    return new Promise((resolve, reject) => {
-        try {
-            eval(src)
-            resolve();
-        } catch (e) {
-            console.error(e);
-            reject();
-        }
-    })
-}
-
 module.exports = {
     writeFile: writeFile,
     getFile: getFile,
-    evalSrc: evalSrc,
     getAllKeys: getAllKeys
 }
