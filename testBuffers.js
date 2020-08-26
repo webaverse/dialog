eadd905fcedae0765e04e482162ec39e96274921
Chris <chris.eddy.dev@gmail.com>
2020-08-25 22:45:32 -0700

    taskId more buffer work, better peer notifications

--- testBuffers.js
+++ testBuffers.js
@@ -1,7 +1,12 @@
 edit(['hi.bin', 'lol.bin', 'haha.bin', 'meme.bin'], buffers => {
-    let newBuffers = []
-    buffers.forEach((buffer) => {
-        newBuffers.push(new Buffer(buffers[0])[2] = 7);
-    })
-    return newBuffers
+    let newBuffers = [];
+    for (let i = 0; i < buffers.length; i++) {
+        const buffer = buffers[i];
+        if (buffer) {
+            newBuffers.push(new Buffer(buffer)[2] = 7);
+        } else {
+            newBuffers.push(new Buffer.alloc(10));
+        }
+    }
+    return newBuffers;
 })
